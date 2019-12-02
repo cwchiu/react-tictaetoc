c49b36978fa1c7bed924c9817b9be7c550dc9bac
Arick <wang2019031@gmail.com>
2019-12-02 17:13:47 +0800

    Updates

---
+++ precache-manifest.73dfa5b33b03c4bd284c3cc1d7e6b571.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b09c559184fbe7dfeb9648955156c3",
+    "url": "/react-tictaetoc/index.html"
+  },
+  {
+    "revision": "020eb1ab5307d38bf7ae",
+    "url": "/react-tictaetoc/static/css/main.b209d603.chunk.css"
+  },
+  {
+    "revision": "f39b88a6fac7a5fe4e4e",
+    "url": "/react-tictaetoc/static/js/2.6847a472.chunk.js"
+  },
+  {
+    "revision": "020eb1ab5307d38bf7ae",
+    "url": "/react-tictaetoc/static/js/main.706cc9d1.chunk.js"
+  },
+  {
+    "revision": "54ea11b132fe9f9ae0bd",
+    "url": "/react-tictaetoc/static/js/runtime-main.98189573.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-tictaetoc/static/media/logo.25bf045c.svg"
+  }
+]);
